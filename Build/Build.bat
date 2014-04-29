33755d90a36d219d5782021d21a40fa5fb0539f9
PassP <deviceobject@gmail.com>
2014-04-29 15:51:22 +0800

    add nasm

--- Build/Build.bat
+++ Build/Build.bat
@@ -1,4 +1,4 @@
 @echo -----------------------------------
-nasm -f bin ../Source/Bootkit.asm -o ../Bin/Bootkit.bin
+nasm-0.98.36-win32\nasmw.exe -f bin ../Source/Bootkit.asm -o ../Bin/Bootkit.bin
 @echo -----------------------------------
 pause
